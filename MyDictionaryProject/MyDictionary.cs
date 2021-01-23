773eb5b54925fbb17a83575147a254247c5088f8
AEren46 <77582721+AEren46@users.noreply.github.com>
2021-01-23 19:25:10 +0300

    Proje dosyası ekle.

---
+++ MyDictionaryProject/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryProject
+{
+    class MyDictionary<K,V>
+    {
+        K[] anahtarlar;
+        V[] degerler;
+
+        public MyDictionary()
+        {
+            anahtarlar = new K[0];
+            degerler = new V[0];
+        }
+        public void Add(K anahtar, V deger)
+        {
+            K[] tempanahtarlar = anahtarlar;
+            V[] tempdeğerler = degerler ;
+            anahtarlar = new K[anahtarlar.Length + 1];
+            degerler = new V[degerler.Length + 1];
+            for (int i = 0; i < tempanahtarlar.Length; i++)
+            {
+                anahtarlar[i] = tempanahtarlar[i];
+                degerler[i] = tempdeğerler[i]; 
+            }
+            anahtarlar[anahtarlar.Length - 1] = anahtar;
+            degerler[degerler.Length - 1] = deger;
+
+        }
+        public void Yazdir(int a)
+        {
+            Console.WriteLine(anahtarlar[a]+"   "+ degerler[a]);
+        }
+    }
+}
---
+++ MyDictionaryProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> sehirler = new MyDictionary<int, string>();
+            sehirler.Add(1, "Ankara");
+            sehirler.Add(31, "Hatay");
+            sehirler.Add(46, "Kahramanmaraş");
+            sehirler.Yazdir(0);
+            sehirler.Yazdir(1);
+            sehirler.Yazdir(2);
+        }
+    }
+}
